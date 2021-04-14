046cce4948234a40e7d05ba415d00117f71f8885
zsh <1012352205@qq.com>
2021-04-14 23:19:22 +0800

    Feat: 测试完毕，已知问题都已经解决

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="home">
-    <div class="aaa">点击我生成组件</div>
+    <div class="aaa" @click="clickTest">点击我生成组件</div>
+
+    <div ref="canvas" class="canvas"></div>
   </div>
 </template>
 
 <style scoped lang="less">
   .aaa {
-    position: absolute;
-    top: 120px;
-    left: 400px;
+    width: 120px;
+    margin: 120px 0 0 300px;
     padding: 8px 24px;
     border: 1px solid rgba(0, 0, 0, .5);
     cursor: pointer;
@@ -17,12 +18,38 @@
 </style>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, createApp, ref, reactive, h } from 'vue'
 import HelloWorld from '@/components/HelloWorld.vue'
+
+const classId = 0
 
 export default defineComponent({
   name: 'Home',
-  components: {
+
+  setup () {
+    const canvas = ref<HTMLDivElement>()
+
+    return {
+      canvas,
+      clickTest () {
+        if (!canvas.value) return
+
+        const container = document.createElement('div')
+        const props = reactive({ msg: '123' })
+
+        setTimeout(() => {
+          props.msg = '456'
+        }, 1000)
+
+        container.className = `_container_class_${classId}`
+        createApp({
+          render () {
+            return h(HelloWorld, props)
+          }
+        }).mount(container)
+        canvas.value.appendChild(container)
+      }
+    }
   }
 })
 </script>
