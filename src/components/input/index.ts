3e88c08699cf82704cdff1994897f75fbcbc2466
zhoushaohui <1012352205@qq.com>
2021-06-03 18:43:19 +0800

    feat: 新增input组件，待完善

---
+++ src/components/input/index.ts
@@ -0,0 +1,31 @@
+import { stringProp, numberProp, baseProps, cmtBaseStyle } from '../helper'
+import { computed, defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'ZInput',
+
+    props: {
+        ...baseProps(),
+        height: numberProp(24),
+        text: stringProp('默认文本'),
+        color: stringProp(),
+        fontSize: numberProp(16),
+        fontWeight: numberProp(400),
+        lineHeight: numberProp(24),
+        type: stringProp()
+    },
+
+    setup (props) {
+        return {
+            baseStyle: computed(() => {
+                return {
+                    ...cmtBaseStyle(props).value,
+                    fontSize: `${props.fontSize}px`,
+                    color: props.color,
+                    fontWeight: props.fontWeight,
+                    lineHeight: `${props.lineHeight}px`
+                }
+            })
+        }
+    }
+})
---
+++ src/components/input/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <a-input
+        class="input-component"
+        :style="baseStyle"
+        :value="text"
+        :type="type"
+    />
+</template>
+
+<style lang="less" scoped>
+    .text-component {
+        word-break: break-all;
+    }
+</style>
+
+<script src="./index.ts" lang="ts"></script>
